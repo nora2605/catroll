30e3ec74cb713c4440270269c3ca066cfb4139b8
Nora J.F <nora.ja2605@gmail.com>
2024-01-24 23:50:58 +0100

    Now in-place and totally safe

--- catroll.c
+++ catroll.c
@@ -1,118 +1,77 @@
-#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdint.h>
 #ifdef _WIN32
-#include <windows.h>	// for Sleep()
-#else 
-#include <unistd.h>	// for usleep()
+#include <Windows.h>
+#else
+#include <unistd.h>
 #endif
 
-#define chunk 1024
+enum Mode {
+    FILE_MODE,
+    TEXT_MODE
+};
 
-//joins an array of strings to a single one using a delimeter string
-char *join(char *args[], char *inter) {
-    int i, len;
-    char* result;
-    len = 0;
-    for (i = 0; args[i]; i++)
-        len += strlen(args[i]) + 1;
-    result = malloc(len);
-    result[0] = '\0';
-    for (i=0; args[i]; i++) {
-        result = strcat(result, args[i]);
-        result = strcat(result, inter);
+void cycle(char* text, int length) {
+    char first = text[0];
+    for (int i = 1; i < length; i++) {
+        text[i - 1] = text[i];
     }
-    return result;
+    text[length - 1] = first;
 }
 
-char *cycle(char *str, size_t s) {
-    char *res = malloc(s+1);
-    for (int i = 0; i < s; i++)
-        res[i] = str[(i + 1) % s];
-    res[s] = '\0';
-    free(str);
-    return res;
-}
+int main(int argc, char *argv[]) {
+    enum Mode mode = FILE_MODE;
+    long delay = 100;
 
-int main(int argc, char *argv[])
-{
-    enum { TEXT_MODE, FILE_MODE } input_mode = FILE_MODE;
-    size_t optind;
-    long delay = 100000;
-    char *help_text = "No help available yet.";
-
-    for (optind = 1; optind < argc && argv[optind][0] == '-'; optind++)
-    {
-        switch (argv[optind][1])
-        {
-            case 't': input_mode = TEXT_MODE; break;
-            case 'f': input_mode = FILE_MODE; break;
-            case 'd': 
-                delay = strtol(argv[++optind], NULL, 10);
-                if (!delay) {
-                    fprintf(stderr, "Invalid integer for -d.\n");
-                    exit(EXIT_FAILURE);
-                } else delay *= 1000;
-                break;
-            case 'h':
-                printf(help_text);
-                exit(EXIT_SUCCESS);
-                break;
-            default:
-                fprintf(stderr, "Usage: %s [-tfdh] <input>\nFor more information see %s -h\n", argv[0], argv[0]);
-                exit(EXIT_FAILURE);
-                break;
+    for (int i = 1; i < argc - 1; i++) {
+        if (strcmp(argv[i], "-t") == 0) {
+            mode = TEXT_MODE;
+        } else if (strcmp(argv[i], "-f") == 0) {
+            mode = FILE_MODE;
+        } else if (strcmp(argv[i], "-d") == 0 && i + 1 < argc) {
+            delay = atol(argv[i + 1]);
+            i++;
+        } else if (strcmp(argv[i], "-h") == 0) {
+            printf("Catroll Util.\n\nUsage: catroll [OPTIONS]\n\nOptions:\n\t-t\t\tText input mode\n\t-f\t\tFile input mode\n\t-d DELAY\tDelay in ms\n\t-h\t\tShow this help\n\n");
+            return 0;
+        } else {
+            fprintf(stderr, "Unknown option '%s'\n", argv[i]);
+            return 1;
         }
     }
-
-    if (!*(argv + optind))
-    {
-        fprintf(stderr, "Usage: %s [-tfdh] <input>\nFor more information see %s -h\n", argv[0], argv[0]);
-        exit(EXIT_FAILURE);
-    }
-    argv += optind;
-
-    char *input;
-    if (input_mode == FILE_MODE)
-    {
+    char *text = NULL;
+    if (mode == FILE_MODE) {
+        char *fileName = argv[argc - 1];
         FILE *file;
-        char *buffer = 0;
-        size_t length;
-
-        file = fopen(argv[0], "r");
-        if (file)
-        {
-            fseek(file, 0, SEEK_END);
-            length = ftell(file);
-            fseek(file, 0, SEEK_SET);
-            buffer = malloc(length);
-            if (buffer)
-                fread(buffer, 1, length, file);
-            fclose(file);
+        errno_t err = fopen_s(&file, fileName, "r");
+        if (err != 0) {
+            char errBuffer[80];
+            strerror_s(errBuffer, 80, err);
+            fprintf(stderr, "Can't open file '%s': %s\n", fileName, errBuffer);
+            return 1;
         }
-        else 
-        {
-            fprintf(stderr, "File \"%s\" not found or inaccessible\n", argv[0]);
-            exit(EXIT_FAILURE);
+        fseek(file, 0, SEEK_END);
+        long size = ftell(file);
+        fseek(file, 0, SEEK_SET);
+        text = malloc(size + 1);
+        long i = 0;
+        while (fgets(text + i, size - i, file) != NULL) {
+            i = strlen(text);
         }
-        if (buffer)
-            input = buffer;
-        else
-            exit(EXIT_FAILURE);
+        fclose(file);
+    } else {
+        text = argv[argc - 1];
     }
-    else
-        input = join(argv, " ");
-
-    size_t s = strlen(input);
-    while (true) {
-        printf("%s\n", input);
-        input = cycle(input, s);
+    long length = strlen(text);
+    while (1) {
+        printf("%s", text);
+        cycle(text, length);
         #ifdef _WIN32
-        Sleep(delay/1000);
+        Sleep(delay);
         #else
-        usleep(delay);
+        usleep(delay * 1000);
         #endif
+        printf("\r");
     }
 }
